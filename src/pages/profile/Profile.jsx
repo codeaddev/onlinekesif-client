37fef2bafe45cfe62f14271770b44fd2b6e6f571
nrzkn92 <80405687+nazimozkan92@users.noreply.github.com>
2023-04-14 10:27:40 +0300

    Ok

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,57 +1,59 @@
-import React, { useContext, useEffect } from 'react'
-import "./profile.scss"
-import Navbar from '../../components/Navbar/Navbar'
-import Footer from '../../components/footer/Footer'
-import Sidebar from '../../components/sidebar/Sidebar'
-import Widgets from '../../components/Widgets/Widgets'
-import Hidden from './Hidden'
-import Column from './columns/Column'
-import { CloudContext } from '../../context/cloudContext'
-import { sendMail } from '../../context/mail'
-import BreadCrumb from './BreadCrumb'
-import { isMobile } from 'react-device-detect'
-import ProfileHeaderBar from './ProfileHeaderBar'
+import React, { useContext } from "react";
+import "./profile.scss";
+import Widgets from "../../components/Widgets/Widgets";
+import Column from "./columns/Column";
+import { CloudContext } from "../../context/cloudContext";
+import { isMobile } from "react-device-detect";
+import ProfileHeaderBar from "./ProfileHeaderBar";
+import SideLinks from "../../components/sidelinks/SideLinks";
 
+function Profile({ setMainList }) {
+  const { myJobs } = useContext(CloudContext);
 
-function Profile({setMainList,}) {
-  const {myJobs}=useContext(CloudContext)
+  const data = [
+    {
+      id: "01",
+      headBar: "Teklif Bekleyen",
+      data: myJobs?.filter((i) => i.Offers.length < 1),
+      svg: "AnyIcon",
+      add: true,
+      emptyMessage: "Henüz teklif bekleyen hizmet talebiniz bulunmamaktadır.",
+    },
+    {
+      id: "02",
+      headBar: "Açık Keşifler",
+      data: myJobs?.filter((i) => i.Offers.length > 0 && !i.completed),
+      svg: "AnyIcon",
+      add: true,
+      emptyMessage: "Henüz açık hizmet talebiniz bulunmamaktadır.",
+      setState: "",
+    },
+    {
+      id: "03",
+      headBar: "Tamamlandı",
+      data: myJobs?.filter((i) => i.completed || i.expired),
+      svg: "AnyIcon",
+      emptyMessage: "Henüz tamamlanan hizmetiniz bulunmamaktadır.",
+    },
+    { id: "04", headBar: "DUYURULAR", data: [], svg: "AnyIcon" },
+  ];
 
-  const data =[
-    
-    {id:"01",headBar:"Teklif Bekleyen", data:myJobs?.filter(i=>i.Offers.length<1),svg:"AnyIcon",add:true,emptyMessage:"Henüz teklif bekleyen hizmet talebiniz bulunmamaktadır."},
-    {id:"02",headBar:"Açık Keşifler", data:myJobs?.filter(i=>i.Offers.length>0&&!i.completed),svg:"AnyIcon",add:true,emptyMessage:"Henüz açık hizmet talebiniz bulunmamaktadır.",setState:"",},
-    {id:"03",headBar:"Tamamlandı", data:myJobs?.filter(i=>i.completed||i.expired),svg:"AnyIcon",emptyMessage:"Henüz tamamlanan hizmetiniz bulunmamaktadır."},
-    {id:"04",headBar:"DUYURULAR", data:[],svg:"AnyIcon"},
-  ]
-
-const pages=[
-  {id:"01",label:"Profil",route:"/profil",link:true}
-]
+  const pages = [{ id: "01", label: "Profil", route: "/profil", link: true }];
   return (
-    <div className='profile'>
-      
+    <div className="profile">
+      <SideLinks />
       <div className="profile-container">
         {/* <Hidden/> */}
-        <Sidebar/>
-
         <div className="profile-inner-container">
-       
-        {/* <BreadCrumb pages={pages}/> */}
-       {isMobile&&<ProfileHeaderBar/>}
+          {/* <BreadCrumb pages={pages}/> */}
+          {isMobile && <ProfileHeaderBar />}
           <div className="top">
-            <Widgets myJobs={myJobs}/>
+            <Widgets myJobs={myJobs} />
           </div>
-          
+
           <div className="bottom">
-            
-            {data.map(i=>{
-              return(
-                <Column 
-                key={i.id} 
-                item={i}
-                setMainList={setMainList}
-                />
-              )
+            {data.map((i) => {
+              return <Column key={i.id} item={i} setMainList={setMainList} />;
             })}
           </div>
           {/* <div 
@@ -61,9 +63,8 @@ const pages=[
           </div> */}
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
